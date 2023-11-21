bf2b12e315983d4dd957a26b0b14eab2f5e97137
Joakim Nohlgård <joakim@nohlgard.se>
2023-11-21 15:06:26 +0100

    Implementing encryption initialization and unlocking

--- mkosi.images/initrd/mkosi.extra/usr/libexec/enroll-yubikey.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/sh
-set -eu
-
-systemd-cryptenroll --wipe-slot=empty --fido2-device=auto --tpm2-public-key=/run/systemd/tpm2-pcr-public-key.pem --tpm2-signature=/run/systemd/tpm2-pcr-signature.json --tpm2-public-key-pcrs=11
